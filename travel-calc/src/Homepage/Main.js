b838b2ad2fbf87bf3768964d3b59b243c098c1d7
tekeufranck681 <tekeufranck681@gmail.com>
2025-01-17 11:55:03 +0100

    second push after initial

---
+++ travel-calc/src/Homepage/Main.js
@@ -0,0 +1,20 @@
+import "./Main.css"
+
+const Main= () => {
+return(
+    <section className="hero">
+<div className="hero-content">
+  <h1>Plan Your Dream Trip Effortlessly!</h1>
+  <p>
+    Track expenses, convert currencies, and create shareable travel budgets with ease.
+  </p>
+  <div className="hero-buttons">
+    <a href="#start-planning" className="primary-btn">Start Planning</a>
+    <a href="#learn-more" className="secondary-btn">Learn More</a>
+  </div>
+</div>
+</section>
+);
+
+}
+export default Main;
